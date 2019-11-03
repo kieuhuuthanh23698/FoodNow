8f7c08fe4fd04a52b175c7f7b418b28f9d0aa414
dainguyen0907 <50541129+dainguyen0907@users.noreply.github.com>
2019-11-03 14:39:03 +0700

    Create KHACH_HANG.js

---
+++ Nodejs/SERVER_1/Models/KHACH_HANG.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const khachhangSchema = new mongoose.Schema({
+    Ten_khach_hang: String,
+    Tai_khoan: String,
+    Mat_khau: String,
+    Don_hang_id: [{type:mongoose.Types.ObjectId}],
+    
+});
+
+module.exports = mongoose.model("KHACH_HANG", khachhangSchema);
