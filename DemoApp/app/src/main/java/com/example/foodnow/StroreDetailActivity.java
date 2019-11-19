d7af52235d35002f05819546cc5b1f2fb747c023
dainguyen0907 <50541129+dainguyen0907@users.noreply.github.com>
2019-11-19 18:11:27 +0700

    main

---
+++ DemoApp/app/src/main/java/com/example/foodnow/StroreDetailActivity.java
@@ -0,0 +1,17 @@
+package com.example.foodnow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StroreDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_strore_detail);
+
+        getSupportActionBar().setTitle("Cửa hàng Phúc Long");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
