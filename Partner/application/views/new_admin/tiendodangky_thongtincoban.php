95ef99019fb8494c75d34e7446745408c5038974
Kiều Hữu Thành <34344168+kieuhuuthanh23698@users.noreply.github.com>
2020-07-04 21:54:18 +0700

    Update tiendodangky_thongtincoban.php

--- Partner/application/views/new_admin/tiendodangky_thongtincoban.php
+++ Partner/application/views/new_admin/tiendodangky_thongtincoban.php
@@ -5,13 +5,28 @@
     border-top-color: aquamarine;
   }
   .step-content{
-     text-align: center;
+    text-align: center;
   }
   .step-icon{
     margin: 10px;
     padding: 0px;
   }
+  #map{
+    background-color: blue;
+    height: 300px;
+    width: 100%;
+    margin: 10px 0;
+  }
 </style>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBpfe5LCma0AfpfNrbJlqxSCOLvOQGQDTg&callback=initMap" async defer></script>
+<script type="text/javascript">
+  function initMap() {
+      var map = new google.maps.Map(document.getElementById("map"), 
+        {center: { lat: 21.0168864, lng: 105.7855574 },zoom: 15});
+      var latLng = { lat: 21.0168864, lng: 105.7855574 }
+      var marker = new google.maps.Marker({position: latLng,map: map,});
+  } 
+</script>
 <div class="content-wrapper">
   <!-- Content Header (Page header) -->
   <div class="content-header">
@@ -86,207 +101,204 @@
                                   Mô tả
                                 </label>
                               </h3>
-                                                                              <!--   <div class="card-tools">
-                                                                                    <button type="button" class="btn btn-tool btn-sm" data-card-widget="collapse" data-toggle="tooltip"
-                                                                                        title="Collapse">
-                                                                                        <i class="fas fa-minus"></i></button>
-                                                                                    <button type="button" class="btn btn-tool btn-sm" data-card-widget="remove" data-toggle="tooltip"
-                                                                                        title="Remove">
-                                                                                        <i class="fas fa-times"></i></button>
-                                                                                      </div> -->
+<!--   <div class="card-tools">
+<button type="button" class="btn btn-tool btn-sm" data-card-widget="collapse" data-toggle="tooltip"
+title="Collapse">
+<i class="fas fa-minus"></i></button>
+<button type="button" class="btn btn-tool btn-sm" data-card-widget="remove" data-toggle="tooltip"
+title="Remove">
+<i class="fas fa-times"></i></button>
+</div> -->
 
-                                                                                      <!-- </div> -->
+<!-- </div> -->
 
-                                                                                      <div class="mb-3">
-                                                                                        <textarea id="mota_km" placeholder="Place some text here"
-                                                                                        style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
-                                                                                      </div>
-                                                                                    </div>
-                                                                                  </div>
-                                                                                  <div class="form-group">
-                                                                                    <label for="exampleInputEmail1">Hình ảnh cửa hàng</label>
-                                                                                    <img id="img_upload">
-                                                                                    <div class="custom-file">
+<div class="mb-3">
+  <textarea id="mota_km" placeholder="Place some text here"
+  style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
+</div>
+</div>
+</div>
+<div class="form-group">
+  <label for="exampleInputEmail1">Hình ảnh cửa hàng</label>
+  <img id="img_upload">
+  <div class="custom-file">
 
-                                                                                      <input type="file" class="custom-file-input" id="inputGroupFile02" />
-                                                                                      <label class="custom-file-label" for="inputGroupFile02" id="Chonfile">Chọn hình ảnh</label>
-                                                                                    </div>
+    <input type="file" class="custom-file-input" id="inputGroupFile02" />
+    <label class="custom-file-label" for="inputGroupFile02" id="Chonfile">Chọn hình ảnh</label>
+  </div>
 
-                                                                                  </div>
+</div>
 
-                                                                                </div>
+</div>
 
-                                                                              </div>
+</div>
 
-                                                                            </section>
+</section>
 
-                                                                          </div>
-
-
-                                                                        </div>
-                                                                      </div>
+</div>
 
 
-
-                                                                      <label>Cấp tài khoản cửa hàng</label>
-                                                                      <div class="card">
-                                                                       <div class="card-body">
-                                                                        <div class="form-group">
-
-                                                                          <div class="form-group">
-                                                                            <label for="exampleInputEmail1">Tên đăng nhập</label>
-                                                                            <input type="email" class="form-control" id="tenDN" aria-describedby="emailHelp" placeholder="Tên đăng nhập sẽ cho tự động mỗi lần click Thêm cửa hàng">
-                                                                            <label for="exampleInputEmail1">Mật khẩu</label>
-                                                                            <input type="email" class="form-control" id="matkhau" aria-describedby="emailHelp" placeholder="Mật khẩu cũng sẽ tự động theo Tên đăng nhập">
-                                                                          </div>
-                                                                        </div>
-                                                                      </div> 
-                                                                    </div>
-
-
-                                                                  </div>
-                                                                  <div class="modal-footer justify-content-between">
-                                                                    <button type="button" class="btn btn-primary float-right">Lưu</button>
-                                                                  </div>
-                                                                </div>
-                                                                <!-- /.modal-content -->
-                                                              </div>
-                                                              <!-- /.modal-dialog -->
-                                                            </div>
-                                                            <!-- /.modal -->
-
-
-                                                          </div>
-
-                                                        </div>
-                                                        <!-- /.card-header -->
-                                                        <div class="row">
-
-                                                          <section class="col-lg-12 connectedSortable">
-
-                                                            <div class="card">
-                                                              <div class="card-header">
-                                                                <h3 class="card-title"> Thông tin cơ bản</h3>
-                                                                <div class="card-tools">
-                                                                  <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                                                                    <i class="fas fa-minus"></i>
-                                                                  </button>
-                  <!-- <button type="button" class="btn btn-tool" data-card-widget="remove">
-                    <i class="fas fa-times"></i>
-                  </button> -->
-                </div>
-              </div>
-              <div class="card-body">
-                <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4">
-                  <!-- <form> -->
-
-                    <div class="row">
-                      <div class="form-group  col-sm-6">
-                        <label for="exampleInputPassword1">Khu vực</label>
-                        <select class="form-control">
-                          <option>TP.HCM</option>
-                          <option>Món 2</option>
-                          <option>Món 3</option>
-                          <option>Món 4</option>
-                          <option>Món 5</option>
-                        </select>
-                      </div>
-                    </div>
-
-                    <div class="row">
-                      <div class="form-group col-sm-6">
-                        <label for="exampleInputEmail1">Tên cửa hàng</label>
-                        <input type="email" class="form-control" id="tenCH" aria-describedby="emailHelp" placeholder="Cơm Đùi Gà Chiên">
-                      </div>
-                      <div class="form-group col-sm-6">
-                        <label for="exampleInputPassword1">Số điện thoại</label>
-                        <input type="text" class="form-control" id="diachiCH">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label for="exampleInputPassword1">Địa chỉ</label>
-                      <input type="text" class="form-control" id="diachiCH">
-                    </div>
-
-                    <div class="form-group">
-                      <button  type="button" class="btn  btn-success"><i class="fas fa-search-location"></i>Tìm trên bản đồ</button>
-                    </div>
+</div>
+</div>
 
 
 
-                  </div>    
+<label>Cấp tài khoản cửa hàng</label>
+<div class="card">
+  <div class="card-body">
+    <div class="form-group">
 
-                </div>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Tên đăng nhập</label>
+        <input type="email" class="form-control" id="tenDN" aria-describedby="emailHelp" placeholder="Tên đăng nhập sẽ cho tự động mỗi lần click Thêm cửa hàng">
+        <label for="exampleInputEmail1">Mật khẩu</label>
+        <input type="email" class="form-control" id="matkhau" aria-describedby="emailHelp" placeholder="Mật khẩu cũng sẽ tự động theo Tên đăng nhập">
+      </div>
+    </div>
+  </div> 
+</div>
 
-                <div class="card-footer">
 
-                  <div class="justify-content-between">
-                    <a href="<?php echo base_url();?>Quanly_danhmuc/homeDanhmuc_dangkycuahang_TTNDD"><button type="button" class="btn btn-primary float-right">Lưu</button></a>
-                  </div>
-                  <div class="justify-content-between">
-                    <a href="<?php echo base_url();?>Quanly_danhmuc/homeDanhmuc_cuahang"><button type="button" class="btn btn-default float-right" style="margin-right: 20px">Quay lại</button></a>
-                  </div>
-                </div>
-              </div>
-            </section>     
+</div>
+<div class="modal-footer justify-content-between">
+  <button type="button" class="btn btn-primary float-right">Lưu</button>
+</div>
+</div>
+<!-- /.modal-content -->
+</div>
+<!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
 
-          </div>
 
+</div>
+
+</div>
+<!-- /.card-header -->
+<div class="row">
+
+  <section class="col-lg-12 connectedSortable">
+
+    <div class="card">
+      <div class="card-header">
+        <h3 class="card-title"> Thông tin cơ bản</h3>
+        <div class="card-tools">
+          <button type="button" class="btn btn-tool" data-card-widget="collapse">
+            <i class="fas fa-minus"></i>
+          </button>
+<!-- <button type="button" class="btn btn-tool" data-card-widget="remove">
+<i class="fas fa-times"></i>
+</button> -->
+</div>
+</div>
+<div class="card-body">
+  <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4">
+    <!-- <form> -->
+
+      <div class="row">
+        <div class="form-group  col-sm-6">
+          <label for="exampleInputPassword1">Khu vực</label>
+          <select class="form-control">
+            <option>TP.HCM</option>
+            <option>Món 2</option>
+            <option>Món 3</option>
+            <option>Món 4</option>
+            <option>Món 5</option>
+          </select>
         </div>
+      </div>
+
+      <div class="row">
+        <div class="form-group col-sm-6">
+          <label for="exampleInputEmail1">Tên cửa hàng</label>
+          <input type="email" class="form-control" id="tenCH" aria-describedby="emailHelp" placeholder="Cơm Đùi Gà Chiên">
+        </div>
+        <div class="form-group col-sm-6">
+          <label for="exampleInputPassword1">Số điện thoại</label>
+          <input type="text" class="form-control" id="diachiCH">
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Địa chỉ</label>
+        <input type="text" class="form-control" id="diachiCH">
+      </div>
+      <div id="map"></div>
+      <div class="form-group">
+        <button  type="button" class="btn  btn-success"><i class="fas fa-search-location"></i>Tìm trên bản đồ</button>
+      </div>
+    </div>    
+
+  </div>
+
+  <div class="card-footer">
+
+    <div class="justify-content-between">
+      <a href="<?php echo base_url();?>Quanly_danhmuc/homeDanhmuc_dangkycuahang_TTNDD"><button type="button" class="btn btn-primary float-right">Lưu</button></a>
+    </div>
+    <div class="justify-content-between">
+      <a href="<?php echo base_url();?>Quanly_danhmuc/homeDanhmuc_cuahang"><button type="button" class="btn btn-default float-right" style="margin-right: 20px">Quay lại</button></a>
+    </div>
+  </div>
+</div>
+</section>     
+
+</div>
+
+</div>
 
 
-        <aside class="control-sidebar control-sidebar-dark">
-          <!-- Control sidebar content goes here -->
-        </aside>
-        <!-- /.control-sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+  <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
 
-      </div >
+</div >
 
-      <style type="text/css">
+<style type="text/css">
 
-        .giatien{
-          font - size: 100%;
-          vertical-align: center;
-          background-color: #A5DF00;
-        }
+  .giatien{
+    font - size: 100%;
+    vertical-align: center;
+    background-color: #A5DF00;
+  }
 
-        .chitiet{
-          padding: 3px 12px 3px 12px;
-        }
+  .chitiet{
+    padding: 3px 12px 3px 12px;
+  }
 
-        #img_upload{
-          width: 100px;
-        }
+  #img_upload{
+    width: 100px;
+  }
 
-        .img_chinhanh{
-          width: 5.5rem;
-          border-radius: 20%;
-        }
+  .img_chinhanh{
+    width: 5.5rem;
+    border-radius: 20%;
+  }
 
-        .card-primary-chinhanh{
-          background - color: #17a2b8;
-          color: #FFFFFF;
-        }
+  .card-primary-chinhanh{
+    background - color: #17a2b8;
+    color: #FFFFFF;
+  }
 
-        .card-primary-cuahang{
-          background - color: #e0a800;
-          color: #FFFFFF;
+  .card-primary-cuahang{
+    background - color: #e0a800;
+    color: #FFFFFF;
 
-        }
-      </style>
+  }
+</style>
 
-      <script>
-        var hovering = false;
-        $('#inputGroupFile02').on('change',function(e){
-                //get the file name
-                var fileName = e.target.files[0].name;
-                //replace the "Choose a file" label
-                //$(this).next('.custom-file-label').html(fileName);
-                var img = document.getElementById('img_upload');
-                img.src = URL.createObjectURL( e.target.files[0]);
-                img.height = "100";
-                document.getElementById("Chonfile").innerHTML=fileName;
-              })
-            </script>
+<script>
+  var hovering = false;
+  $('#inputGroupFile02').on('change',function(e){
+//get the file name
+var fileName = e.target.files[0].name;
+//replace the "Choose a file" label
+//$(this).next('.custom-file-label').html(fileName);
+var img = document.getElementById('img_upload');
+img.src = URL.createObjectURL( e.target.files[0]);
+img.height = "100";
+document.getElementById("Chonfile").innerHTML=fileName;
+})
+</script>
 
 
